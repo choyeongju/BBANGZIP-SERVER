26f3bfab6312f946d3a46572810629ad571fead1
choyeongju <jjudy1301@naver.com>
2025-04-06 20:29:46 +0900

    [init] setting

---
+++ src/main/java/org/sopt/bbangzip/BbangzipApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.bbangzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbangzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/bbangzip/BbangzipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.bbangzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbangzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
